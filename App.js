3fd8bfc143800a210ddee5cfd6f90dfe49af017b
Edu Nievas <114632025+EDUNI11@users.noreply.github.com>
2025-01-29 12:24:00 +0100

    Deleted unused import

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import "./global.css"
 import {SafeAreaProvider} from "react-native-safe-area-context";
 import Constants from "expo-constants";

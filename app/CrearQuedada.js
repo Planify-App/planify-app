d41d9d9b3c68d7fc5ed75a2b510665c2e6732059
eduar <eduardojosue.cerrato@institutvidreres.cat>
2025-02-13 12:15:12 +0100

    Quedada casi completa

--- app/CrearQuedada.js
+++ app/CrearQuedada.js
@@ -1,53 +1,80 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import DatePicker from 'react-datepicker';
-import "react-datepicker/dist/react-datepicker.css";
+import {Platform, StyleSheet, Text, View, TouchableOpacity, TextInput} from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import { Feather } from '@expo/vector-icons';
+import {useNavigation} from "@react-navigation/native";
 
 export default function CrearQuedada() {
+    const navigation = useNavigation();
+
     const [startDate, setStartDate] = useState(new Date());
     const [startTime, setStartTime] = useState(new Date());
 
     const [endDate, setEndDate] = useState(new Date());
-    const [endTime, setEndTime] = useState(new Date(new Date().setHours(23, 59)));
+    const [endTime, setEndTime] = useState(new Date(new Date().setHours(23, 59))); // Hora 23:59 por defecto
+
     const [showStartDatePicker, setShowStartDatePicker] = useState(false);
     const [showStartTimePicker, setShowStartTimePicker] = useState(false);
     const [showEndDatePicker, setShowEndDatePicker] = useState(false);
     const [showEndTimePicker, setShowEndTimePicker] = useState(false);
-    const [isMultiDay, setIsMultiDay] = useState(false);
+    const [isMultiDay, setIsMultiDay] = useState(false); // Estado de la casilla de "más de un día"
 
+    const [startText, setStartText] = useState('Selecciona fecha y hora de inicio');
+    const [endText, setEndText] = useState(`Selecciona fecha y hora de finalización ${endTime}`);
 
-    const onChangeStartDate = (date) => {
-        if (date) {
-            setStartDate(date);
-            updateStartText(date, startTime);
+    const [campoNombreQuedada, nombreQuedada] = useState('');
+    const [campoDescripcion, descripcion] = useState('');
+
+    const onChangeStartDate = (event, selectedDate) => {
+        if (selectedDate) {
+            setStartDate(selectedDate);
+            updateStartText(selectedDate, startTime);
+            if (Platform.OS === 'android') {
+                setShowStartDatePicker(false);
+            }
         }
     };
 
-    const onChangeStartTime = (time) => {
-        if (time) {
-            setStartTime(time);
-            updateStartText(startDate, time);
+    const onChangeStartTime = (event, selectedTime) => {
+        if (selectedTime) {
+            setStartTime(selectedTime);
+            updateStartText(startDate, selectedTime);
+            if (Platform.OS === 'android') {
+                setShowStartTimePicker(false);
+            }
         }
     };
 
-    const onChangeEndDate = (date) => {
-        if (date) {
-            setEndDate(date);
-            updateEndText(date, endTime);
+    const onChangeEndDate = (event, selectedDate) => {
+        if (selectedDate) {
+            setEndDate(selectedDate);
+            updateEndText(selectedDate, endTime);
+            if (Platform.OS === 'android') {
+                setShowEndDatePicker(false);
+            }
         }
     };
 
-    const onChangeEndTime = (time) => {
-        if (time) {
-            setEndTime(time);
-            updateEndText(endDate, time);
+    const onChangeEndTime = (event, selectedTime) => {
+        if (selectedTime) {
+            setEndTime(selectedTime);
+            updateEndText(endDate, selectedTime);
+            if (Platform.OS === 'android') {
+                setShowEndTimePicker(false);
+            }
         }
     };
 
     const updateStartText = (selectedDate, selectedTime) => {
         let formattedDate = `${selectedDate.getDate()}/${selectedDate.getMonth() + 1}/${selectedDate.getFullYear()}`;
         let formattedTime = `Horas: ${selectedTime.getHours()} | Minutos: ${selectedTime.getMinutes()}`;
+        setStartText(`${formattedDate}\n${formattedTime}`);
+    };
+
+    const updateEndText = (selectedDate, selectedTime) => {
+        let formattedDate = `${selectedDate.getDate()}/${selectedDate.getMonth() + 1}/${selectedDate.getFullYear()}`;
+        let formattedTime = `Horas: ${selectedTime.getHours()} | Minutos: ${selectedTime.getMinutes()}`;
+        setEndText(`${formattedDate}\n${formattedTime}`);
     };
 
     const handleBoxPress = () => {
@@ -63,9 +90,17 @@ export default function CrearQuedada() {
 
     return (
         <View style={styles.container}>
+
+            <Text className="mt-2 font-bold">Nombre de la quedada:</Text>
+            <TextInput
+                className="w-72 lg:w-full bg-white/60"
+                style={styles.input}
+                value={campoNombreQuedada}
+                onChangeText={nombreQuedada}
+            />
+
             <Text style={styles.text}>¿Más de un día?</Text>
             <TouchableOpacity style={styles.checkboxContainer} onPress={handleBoxPress}>
-                <Text style={styles.checkboxLabel}>Marcar opción</Text>
                 {isMultiDay ? (
                     <Feather name="check-circle" size={24} color="green" />
                 ) : (
@@ -75,71 +110,151 @@ export default function CrearQuedada() {
 
             <Text style={styles.text}>Fecha y hora de inicio</Text>
 
-            <>
-                <TouchableOpacity style={styles.button} onPress={() => setShowStartDatePicker(true)}>
-                    <Text style={styles.buttonText}>Fecha de inicio</Text>
-                </TouchableOpacity>
+            {Platform.OS === 'android' ? (
+                <>
+                    <TouchableOpacity style={styles.button} onPress={() => setShowStartDatePicker(true)}>
+                        <Text style={styles.buttonText}>Fecha de inicio</Text>
+                    </TouchableOpacity>
 
-                {showStartDatePicker && (
-                    <DatePicker
-                        selected={startDate}
-                        onChange={onChangeStartDate}
-                        dateFormat="dd/MM/yyyy"
-                    />
-                )}
+                    {showStartDatePicker && (
+                        <DateTimePicker
+                            value={startDate}
+                            mode="date"
+                            display="default"
+                            locale="es-ES"
+                            onChange={onChangeStartDate}
+                        />
+                    )}
 
-                <TouchableOpacity style={styles.button} onPress={() => setShowStartTimePicker(true)}>
-                    <Text style={styles.buttonText}>Hora de Inicio</Text>
-                </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => setShowStartTimePicker(true)}>
+                        <Text style={styles.buttonText}>Hora de Inicio</Text>
+                    </TouchableOpacity>
 
-                {showStartTimePicker && (
-                    <DatePicker
-                        selected={startTime}
-                        onChange={onChangeStartTime}
-                        showTimeSelect
-                        showTimeSelectOnly
-                        timeFormat="HH:mm"
-                        timeIntervals={15}
-                    />
-                )}
-            </>
+                    {showStartTimePicker && (
+                        <DateTimePicker
+                            value={startTime}
+                            mode="time"
+                            locale="es-ES"
+                            is24Hour={true}
+                            display="default"
+                            onChange={onChangeStartTime}
+                        />
+                    )}
+                </>
+            ) : (
+                <>
+                    <View style={styles.pickerContainer}>
+                        <View style={styles.dateRow}>
+                            <Text style={styles.pickerLabel}>Fecha de Inicio:</Text>
+                            <DateTimePicker
+                                value={startDate}
+                                mode="date"
+                                display="default"
+                                locale="es-ES"
+                                onChange={onChangeStartDate}
+                            />
+                        </View>
+
+                        <View style={styles.dateRow}>
+                            <Text style={styles.pickerLabel}>Hora de Inicio:</Text>
+                            <DateTimePicker
+                                value={startTime}
+                                mode="time"
+                                locale="es-ES"
+                                is24Hour={true}
+                                display="default"
+                                onChange={onChangeStartTime}
+                            />
+                        </View>
+                    </View>
+                </>
+            )}
 
             {isMultiDay && (
                 <>
                     <Text style={styles.text}>Fecha y hora de finalización</Text>
 
-                    <>
-                        <TouchableOpacity style={styles.button} onPress={() => setShowEndDatePicker(true)}>
-                            <Text style={styles.buttonText}>Fecha de finalización</Text>
-                        </TouchableOpacity>
+                    {Platform.OS === 'android' ? (
+                        <>
+                            <TouchableOpacity style={styles.button} onPress={() => setShowEndDatePicker(true)}>
+                                <Text style={styles.buttonText}>Fecha de finalización</Text>
+                            </TouchableOpacity>
 
-                        {showEndDatePicker && (
-                            <DatePicker
-                                selected={endDate}
-                                onChange={onChangeEndDate}
-                                dateFormat="dd/MM/yyyy"
-                            />
-                        )}
+                            {showEndDatePicker && (
+                                <DateTimePicker
+                                    value={endDate}
+                                    mode="date"
+                                    display="default"
+                                    locale="es-ES"
+                                    onChange={onChangeEndDate}
+                                />
+                            )}
 
-                        <TouchableOpacity style={styles.button} onPress={() => setShowEndTimePicker(true)}>
-                            <Text style={styles.buttonText}>Hora de finalización</Text>
-                        </TouchableOpacity>
+                            <TouchableOpacity style={styles.button} onPress={() => setShowEndTimePicker(true)}>
+                                <Text style={styles.buttonText}>Hora de finalización</Text>
+                            </TouchableOpacity>
 
-                        {showEndTimePicker && (
-                            <DatePicker
-                                selected={endTime}
-                                onChange={onChangeEndTime}
-                                showTimeSelect
-                                showTimeSelectOnly
-                                timeFormat="HH:mm"
-                                timeIntervals={15}
-                            />
-                        )}
-                        <Text style={styles.text}>Hora de finalización: {endTimeString}</Text>
+                            {showEndTimePicker && (
+                                <DateTimePicker
+                                    value={endTime}
+                                    mode="time"
+                                    locale="es-ES"
+                                    is24Hour={true}
+                                    display="default"
+                                    onChange={onChangeEndTime}
+                                />
+                            )}
+                        </>
 
-                    </>
+                    ) : (
+                        <>
+                            <View style={styles.pickerContainer}>
+                                <View style={styles.dateRow}>
+                                    <Text style={styles.pickerLabel}>Fecha de Finalización:</Text>
+                                    <DateTimePicker
+                                        value={endDate}
+                                        minimumDate={startDate}
+                                        mode="date"
+                                        display="default"
+                                        locale="es-ES"
+                                        onChange={onChangeEndDate}
+                                    />
+                                </View>
+
+                                <View style={styles.dateRow}>
+                                    <Text style={styles.pickerLabel}>Hora de Finalización:</Text>
+                                    <DateTimePicker
+                                        value={endTime}
+                                        mode="time"
+                                        locale="es-ES"
+                                        is24Hour={true}
+                                        display="default"
+                                        onChange={onChangeEndTime}
+                                    />
+                                </View>
+                            </View>
+
+                        </>
+                    )}
+
                 </>
             )}
+
+            <Text className="mt-2 font-bold">Descripción:</Text>
+            <TextInput
+                className="w-72 lg:w-full bg-white/60"
+                style={styles.desc_input}
+                value={campoDescripcion}
+                onChangeText={descripcion}
+            />
+
+            <TouchableOpacity style={styles.accept_button}>
+                <Text style={styles.buttonText}>Guardar</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.cancel_button} onPress={() => navigation.navigate('MenuNoLog')}>
+                    <Text style={styles.buttonText}>Cancelar</Text>
+            </TouchableOpacity>
         </View>
     );
 }
@@ -194,5 +309,43 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         marginBottom: 20,
+    },
+    input: {
+        height: 40,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderRadius: 4,
+        paddingHorizontal: 8,
+        marginVertical: 5,
+        backgroundColor: '#fff',
+    },
+    desc_input: {
+        height: 100,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderRadius: 4,
+        paddingHorizontal: 8,
+        marginTop: 4,
+        backgroundColor: '#fff',
+    },
+    accept_button:{
+        flexDirection: "row",
+        backgroundColor: "#007AFF",
+        padding: 10,
+        borderRadius: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        marginVertical: 10,
+        width: 220,
+    },
+    cancel_button:{
+        flexDirection: "row",
+        backgroundColor: "#007AFF",
+        padding: 10,
+        borderRadius: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        marginVertical: 10,
+        width: 150,
     },
 });

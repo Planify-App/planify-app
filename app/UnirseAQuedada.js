40e993e3287386d6584cfa0ff571252a633bfc23
Edu Nievas <114632025+EDUNI11@users.noreply.github.com>
2025-02-19 11:38:10 +0100

    Join hangout

--- app/UnirseAQuedada.js
+++ app/UnirseAQuedada.js
@@ -1,12 +1,74 @@
-import React from 'react';
-import {View, Text, StyleSheet, Button, TextInput} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, TextInput, TouchableOpacity, Alert} from 'react-native';
 
 export default function UnirseAQuedada({ navigation }){
+    const [invitationCode, setInvitationCode] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleJoinQuedada = async () => {
+        setErrorMessage('');
+
+        if (!invitationCode) {
+            setErrorMessage("Por favor, introduce un código de invitación.");
+            return;
+        }
+
+        try {
+            const correo = 'correo_del_usuario@example.com';
+            const response = await fetch('http://localhost:3080/api/joinHangout', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    correo: "ed@d.com",
+                    code: invitationCode,
+                }),
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.message || "Error al unirse a la quedada");
+            }
+
+            const data = await response.json();
+            if (data.status) {
+                Alert.alert("Éxito", data.message, [
+                    {
+                        text: "OK",
+                        onPress: () => navigation.navigate('InicioQuedadas'),
+                    },
+                ]);
+            } else {
+                setErrorMessage(data.message);
+            }
+
+        } catch (error) {
+            console.error("Error:", error);
+            setErrorMessage(error.message);
+        }
+    };
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Codigo de acceso</Text>
-            <TextInput style={styles.input}  />
-            <Button title={"Entrar"} />
+        <View style={styles.container} className="w-full min-h-full lg:min-h-screen bg-[#DBF3EF] pb-10">
+            <View className="max-w-[95%] mx-auto">
+                <Text className="my-5 text-center text-balance font-bold text-4xl">Código de Invitación:</Text>
+            </View>
+            <TextInput
+                className="w-72 lg:w-48 bg-white/60"
+                style={styles.input}
+                placeholder="f2TwER6i"
+                value={invitationCode}
+                onChangeText={setInvitationCode}
+            />
+            {errorMessage && <Text style={styles.errorText}>{errorMessage}</Text>} {/* Display error message */}
+            <TouchableOpacity
+                className="mt-10 bg-[#2C7067] border-[#2C7067] border-2 py-4 lg:py-2 px-8 lg:px-4 rounded-lg min-w-48 lg:min-w-42 flex items-center justify-center lg:opacity-80 lg:hover:opacity-100 lg:hover:scale-[1.01] lg:transition-all"
+                onPress={handleJoinQuedada}
+            >
+                <Text className="text-white font-semibold">
+                    Entrar
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 }
@@ -16,7 +78,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#f0f0f0',
     },
     title: {
         fontSize: 24,
@@ -24,10 +85,10 @@ const styles = StyleSheet.create({
     },
     input: {
         height: 40,
-        width: 200,
-        borderColor: 'black',
+        borderColor: '#ccc',
         borderWidth: 1,
-        marginTop: 20,
-        marginBottom: 20,
-    },
+        borderRadius: 4,
+        paddingHorizontal: 8,
+        marginTop: 4,
+    }
 });

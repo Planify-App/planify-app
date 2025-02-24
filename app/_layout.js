c35bb9e335a6715b609b6be58bd5fc24199d8fb9
EDUNI11 <114632025+EDUNI11@users.noreply.github.com>
2025-02-24 22:09:30 +0100

    Principal layout not allowed in chat

--- app/_layout.js
+++ app/_layout.js
@@ -1,8 +1,13 @@
-import {Slot} from "expo-router";
+import {Slot, useSegments} from "expo-router";
 import {View, StyleSheet, ScrollView} from "react-native";
 import Constants from "expo-constants";
 
 export default function Layout() {
+    const segments = useSegments();
+
+    if (segments[0] === "chat") {
+        return <Slot />;
+    }
     return (
         <ScrollView className="bg-[#DBF3EF]">
             <View style={styles.pag}>

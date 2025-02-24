eadd3f31eec0db7337efacb168b3b3698beb6de2
EDUNI11 <114632025+EDUNI11@users.noreply.github.com>
2025-02-24 22:10:18 +0100

    Async storage added to save login info

--- app/index.js
+++ app/index.js
@@ -1,10 +1,28 @@
 import MenuNoLog from "./MenuNoLog";
 import InicioQuedadas from "./InicioQuedadas";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useEffect, useState} from "react";
 
-export default function Index(){
-    //if(localStorage.getItem("loggedIn")) {
-        //return <InicioQuedadas />
-    //} else {
-        return <MenuNoLog />
-    //}
+export default async function Index() {
+        const [isUserLoggedIn, setIsUserLoggedIn] = useState(null);
+
+        useEffect(() => {
+                const checkUserSession = async () => {
+                        try {
+                                const userSession = await AsyncStorage.getItem('userSession');
+                                setIsUserLoggedIn(!!userSession);
+                        } catch (error) {
+                                console.error('Error al verificar la sesión del usuario:', error);
+                                setIsUserLoggedIn(false);
+                        }
+                };
+
+                checkUserSession();
+        }, []);
+
+        if (isUserLoggedIn === null) {
+                return null;
+        }
+
+        return isUserLoggedIn ? <InicioQuedadas /> : <MenuNoLog />;
 }
